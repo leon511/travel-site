558403c2bc58e9d444960afc498367d8e2d7dabc
Le <leon5@mac.comgit config --global user.email leon5@mac.comgit config --global user.email leon5@mac.com>
2020-09-13 11:39:19 -0700

    finished save onscroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,12 @@
 import "../styles/styles.css";
-import MobileMenu from './modules/MobileMenu'
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/Reveal-on-scroll";
+
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
   module.hot.accept();
 }
-
-
